e4267af8ae6f81f684a9b2f09b51e006c10d0fbd
peterlu0ng <peter.luong678@gmail.com>
2024-08-17 17:13:15 +1000

    Write gym migration

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_15_004215) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "gyms", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "address", default: "", null: false
+    t.jsonb "operating_hours", default: {}, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
