2176d28bc830ebedae38258e490249577d94304f
peterlu0ng <peter.luong678@gmail.com>
2024-08-26 00:05:31 +1000

    update migrations to use UUIDs as primary key.

--- db/migrate/20240814063345_create_gyms.rb
+++ db/migrate/20240814063345_create_gyms.rb
@@ -1,10 +1,9 @@
 class CreateGyms < ActiveRecord::Migration[7.1]
   def change
-    create_table :gyms do |t|
+    create_table :gyms, id: :uuid do |t|
       t.string :name, default: '', null: false
       t.string :address, default: '', null: false
       t.jsonb :operating_hours, null: false, default: {}
-
       t.timestamps
     end
   end
