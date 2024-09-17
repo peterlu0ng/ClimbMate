fd54cbde22e4b5f7030585fed40a6206e11e3e13
peterlu0ng <peter.luong678@gmail.com>
2024-09-17 12:02:32 +1000

    write gym migration

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -5,4 +5,7 @@ class Gym < ApplicationRecord
   # Validations
   validates :name, presence: true
   validates :address, presence: true
+
+  # JSONB for handling operating_hours
+  store_accessor :operating_hours, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday
 end
